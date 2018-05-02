3074a9360546ccc22252e3d3f8628c9cdb86a6df
hz-wang <wanghaozhe1994@hotmail.com>
2018-05-03 00:22:01 +0800

    100

---
+++ src/100.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p==null||q==null) return p==q;
+        if (p.val!=q.val) return false;
+        return isSameTreeHelp(p.left,q.left)&&isSameTreeHelp(p.right,q.right);
+    }
+    private boolean isSameTreeHelp(TreeNode p, TreeNode q){
+        if (p==null||q==null) return p==q;
+        if(p.val!=q.val) return false;
+        return isSameTreeHelp(p.left,q.left)&&isSameTreeHelp(p.right,q.right);
+    }
+}
