bc3bc9ac27b25afada105d675c4289e2ae07a7b0
hz-wang <wanghaozhe1994@hotmail.com>
2018-07-21 00:30:19 +0800

    217 other solution improve

--- src/217.java
+++ src/217.java
@@ -9,3 +9,26 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+//Other Solution
+class Solution {
+	public boolean containsDuplicate(int[] nums) {
+	    if(nums.length < 2) return false;
+	    int max=nums[0];
+	    int min=nums[0];
+	    for(int i:nums){
+	        if(i>max) max=i;
+	        if(i<min) min =i;
+	    }
+	    if (max-min+2<nums.length) return true; //My improvement
+	    boolean[] boolNums = new boolean[max-min+2];
+	    for(int i:nums){
+	        if(boolNums[i-min]) return true;
+	        else boolNums[i-min]=true;
+	    }
+	    return false;
+	}
+}
