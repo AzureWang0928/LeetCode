504a587a44bd1616f37f9110faea7813a2110811
hz-wang <wanghaozhe1994@hotmail.com>
2018-07-21 13:24:28 +0800

    203 solution

---
+++ src/203.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head==null) return null;
+        ListNode cur = head;
+        while(cur!=null&&cur.next != null){              
+            if (cur.next.val==val)
+                cur.next=cur.next.next;
+            else//the else judge is so important
+                cur = cur.next;
+        }
+        return head.val==val?head.next:head;
+    }
+}
