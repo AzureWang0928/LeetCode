392ba78d860be2d0415e2170a9a605b42d7c4f03
Haozhe Wang <wanghaozhe1994@hotmail.com>
2018-09-21 12:40:06 -0400

    171 so easy

---
+++ src/171.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int titleToNumber(String s) {
+        int ans = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            ans+=(s.charAt(i)-'A'+1) * Math.pow(26, (s.length()-i-1));
+        }
+        return ans;
+    }
+}
