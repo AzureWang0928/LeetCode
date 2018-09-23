2ecab6ee4f1fa3b6fa4a683a64949b23bc53967c
Haozhe Wang <wanghaozhe1994@hotmail.com>
2018-09-23 01:32:01 -0400

    350 best solution

--- src/350.java
+++ src/350.java
@@ -1,3 +1,33 @@
+//best solution
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i1 = 0, i2 = 0, i = 0;
+        int[] arr = new int[nums1.length > nums2.length ? nums2.length : nums1.length];
+        while (i1 < nums1.length && i2 < nums2.length && i < arr.length) {
+            if (nums1[i1] > nums2[i2]) {
+                i2++;
+            }
+            else if (nums1[i1] < nums2[i2]) {
+                i1++;
+            }
+            else {
+                arr[i] = nums1[i1];
+                i++;
+                i1++;
+                i2++;
+            }
+        }
+        return Arrays.copyOf(arr, i);
+    }
+}
+
+
+//my solution
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         ArrayList<Integer> ans = new ArrayList<Integer>();
