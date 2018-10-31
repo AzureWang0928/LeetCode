ef62b892935e2f16bcd4ff38ed5704ed70923c57
Haozhe Wang <wanghaozhe1994@hotmail.com>
2018-10-30 21:32:54 -0400

    252 best solution

---
+++ src/252.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if(intervals == null){
+            return true;
+        }
+        Arrays.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval a, Interval b){
+                return a.start - b.start;
+            }
+        });
+        for(int i = 0; i + 1 < intervals.length; i++){
+            if(intervals[i].end > intervals[i + 1].start){
+                return false;
+            }
+        }
+        return true;
+    }
+}
