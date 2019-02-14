eeb065acd36489da784d587b5b2e595b4ade77cb
Haozhe Wang <wanghaozhe1994@hotmail.com>
2019-02-14 16:27:41 -0500

    update problem number

--- src/83.java
+++ src/83.java
@@ -9,13 +9,12 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
-        while(cur!=null){
-            if (cur.next==null) break;
-            if (cur.val==cur.next.val){
-                cur.next=cur.next.next;
+        while(cur != null && cur.next != null){
+            if(cur.val == cur.next.val){
+                cur.next = cur.next.next;
             }
             else{
-                cur=cur.next;
+                cur = cur.next;
             }
         }
         return head;
